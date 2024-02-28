49aa4de822e0b0655c0544a69ec032d31a3cbe30
blintdm98@gmail.com <blintdm98@gmail.com>
2024-02-28 18:03:28 +0200

    open-close modal

--- crypto-exchange/src/app/app.module.ts
+++ crypto-exchange/src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CryptoModule } from './crypto/crypto.module';
-import { LoginComponent } from './auth/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavComponent } from './common/nav/nav.component';
 import { ModalComponent } from './common/modal/modal.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- crypto-exchange/src/app/common/nav/nav.component.ts
+++ crypto-exchange/src/app/common/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  isModalOpen = false;
 
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  openModal() {
+    this.isModalOpen = true;
+    this.renderer.setStyle(this.elRef.nativeElement.querySelector('.modal'), 'display', 'block');
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.renderer.setStyle(this.elRef.nativeElement.querySelector('.modal'), 'display', 'none');
+  }
 }
